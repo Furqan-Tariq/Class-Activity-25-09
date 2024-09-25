56cd7bd96266ae519242cb736b0a00b6226f3fda
Furqan-Tariq <furqant.ft@gmail.com>
2024-09-25 16:22:37 +0500

    chrone: testfile

--- calculator.py
+++ calculator.py
@@ -12,7 +12,3 @@ class Calculator:
         if b == 0:
             raise ValueError("Cannot divide by zero.")
         return a / b
-    
-    def test(self,a):
-        print("this is a test functions")
-        return 0
